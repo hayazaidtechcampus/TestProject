cc86227b6bed4ec72c289785138de72577672d0c
922638 <922638@LTSA000371.cts.com>
2021-08-02 22:51:04 +0300

    Changes In Second Branch

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -1,4 +1,11 @@
 
 public class TestClass {
 
+	public static void main(String[] args) {
+		String[] cars = { "Volvo", "BMW", "Ford", "Ford" };
+		for (int i = 0; i < cars.length; i++) {
+			System.out.println(cars[i]);
+		}
+	}
+
 }
